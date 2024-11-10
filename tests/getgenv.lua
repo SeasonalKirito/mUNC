bfb8b7ea5629404467928bcd182b6e9010e0022a
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-11-09 20:21:55 -0700

    Update getgenv.lua

--- tests/getgenv.lua
+++ tests/getgenv.lua
@@ -1,8 +1,8 @@
 if getgenv then
-    if typeof(getgenv) == "function" then
+    if (typeof(getgenv) == "function" and typeof(getgenv()) == "table") then
         return "Mr.Beast's stash of cp(cool pizza(lunchly)) (getgenv is real.)"
     else
-        return "getgenv is not a table"
+        return "getgenv is not a function"
     end
 else
     return "getgenv is not defined"
