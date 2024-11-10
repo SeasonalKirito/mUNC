30228b76e7485dac3914310b40209870d5222a1d
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-11-09 19:38:46 -0700

    Update hootmetamethod.lua

--- tests/hootmetamethod.lua
+++ tests/hootmetamethod.lua
@@ -5,12 +5,21 @@ local originalIndex = debug.getmetatable(object).__index
 local ref = hookmetamethod(object, "__index", function(self, key)
     if key == "test" then
         return true
+    elseif key == "anotherTest" then
+        return "anotherValue"
     else
         return originalIndex(self, key)
     end
 end)
 
-assert(object.test == true, "Failed to hook a metamethod and change the return value")
-assert(ref() == false, "Did not return the original function")
-assert(debug.getmetatable(object).__metatable == "Locked!", "Failed to lock the metatable")
-assert("Sigma patric batemen moment (ur shi works)")
+if object.test ~= true then
+    return "Failed to hook a metamethod and change the return value"
+elseif object.anotherTest ~= "anotherValue" then
+    return "Failed to hook a metamethod and change the return value for anotherTest"
+elseif ref() ~= false then
+    return "Did not return the original function"
+elseif debug.getmetatable(object).__metatable ~= "Locked!" then
+    return "Failed to lock the metatable"
+else
+    return "Sigma patric batemen moment (ur shi works)"
+end
