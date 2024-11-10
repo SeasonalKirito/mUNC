128c3d82c112f7a7f14f4edbbc98343cc057c27c
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-11-09 19:02:02 -0700

    hookmetamethod

--- tests/hootmetamethod.lua
+++ tests/hootmetamethod.lua
@@ -1,13 +1,13 @@
-
+print("Testing hookmetamethod (Walkspeed example)")
 
 local success, error = pcall(function()
     local old
-    old = hookmetamethod(game,"__index",function(self,key)
+    old = hookmetamethod(game,"__index", newcclosure(function(self,key)
         if self == game.Players.LocalPlayer.Character.Humanoid and key == "WalkSpeed" then
             return 10.5
         end
         return old(self,key)
-    end)
+    end))
 end)
 
 if not success then
