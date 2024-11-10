07b83938a75926f4ef2cc4dcb76607f603919a95
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-11-09 21:08:33 -0700

    request

--- source/beta.lua
+++ source/beta.lua
@@ -1,3 +1,4 @@
+
 --<< Variables >>--
 local mUNC = {
     repo = "https://raw.githubusercontent.com/SeasonalKirito/mUNC/main/",
@@ -17,6 +18,9 @@ function mUNC.Functions.getScript(name)
 end
 
 function mUNC.Functions.runScript(info, callback)
+    if not info.Web then
+        callback()
+    end
     local result = loadstring(mUNC.Functions.getScript(info.Name))()
     callback(result)
     return result
@@ -27,9 +31,47 @@ end
 --<< Tests >>--
 mUNC.Functions.runScript(
     {
+        Name = "request",
+        Description = "Tests the request function",
+        Version = "1.0.0",
+        Web = false
+    },
+
+    function()
+        if not (request or http and (http.request or http_request)) then
+            print("Request functions are not available")
+        else
+            local response = request({
+                Url = "https://httpbin.org/user-agent",
+                Method = "GET",
+            })
+            if type(response) ~= "table" then
+                print("Response must be a table")
+            elseif response.StatusCode ~= 200 then
+                print("Did not return a 200 status code")
+            else
+                local data = game:GetService("HttpService"):JSONDecode(response.Body)
+                if type(data) ~= "table" or type(data["user-agent"]) ~= "string" then
+                    print("Did not return a table with a user-agent key")
+                else
+                    print("User-Agent: " .. data["user-agent"])
+                    mUNC.TestedPositive["iscclosure"] = true
+                end
+            end
+        end
+    end
+)
+
+if not mUNC.TestedPositive["iscclosure"] then
+    return
+end
+
+mUNC.Functions.runScript(
+    {
         Name = "print",
         Description = "Tests the print function",
-        Version = "1.0.0"
+        Version = "1.0.0",
+        Web = true
     },
 
     function(result)
@@ -41,7 +83,8 @@ mUNC.Functions.runScript(
     {
         Name = "getgenv",
         Description = "Tests the getgenv table",
-        Version = "1.0.0"
+        Version = "1.0.0",
+        Web = true
     },
 
     function(result)
@@ -53,7 +96,8 @@ mUNC.Functions.runScript(
     {
         Name = "iscclosure",
         Description = "Tests the iscclosure function",
-        Version = "1.0.0"
+        Version = "1.0.0",
+        Web = true
     },
 
     function(result)
@@ -69,7 +113,8 @@ if mUNC.TestedPositive["iscclosure"] then
         {
             Name = "newcclosure",
             Description = "Tests the newcclosure function",
-            Version = "1.0.0"
+            Version = "1.0.0",
+            Web = true
         },
 
         function(result)
@@ -88,7 +133,8 @@ if mUNC.TestedPositive["newcclosure"] then
     {
         Name = "hookmetamethod",
         Description = "Tests the hookmetamethod function",
-        Version = "1.0.0"
+        Version = "1.0.0",
+        Web = true
     },
 
     function(result)
@@ -98,4 +144,3 @@ if mUNC.TestedPositive["newcclosure"] then
 else
     print("newcclosure test failed")
 end
-print("Fuck u lil nigger lovrewe")
